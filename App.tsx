ac4370fc5c9aa81db6e2ae1e3eb9b34079c760ac
chidiogo-akunne <chidiogoakunne@gmail.com>
2020-11-19 20:55:03 +0100

    load resources before entering app

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { enableScreens } from "react-native-screens";
+import AppLoading from "./src/libs/appLoading";
+import AppRouter from "./src";
+
+enableScreens();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isAppReady, setIsAppReady] = useState(false);
+
+  return isAppReady ? (
+    <AppRouter />
+  ) : (
+    <AppLoading setIsAppReady={setIsAppReady(true)} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
